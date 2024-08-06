5e7f0dddd275e83d33265e551d825ff42dbff4f7
aplanillo <aimara.planillo@gmail.com>
2024-08-06 16:11:36 +0200

    Updated
    removed extra lines
    added package patchwork to source_packages

--- rscripts/source_packages.R
+++ rscripts/source_packages.R
@@ -1,5 +1,5 @@
 mylibraries <- c("dplyr", "vegan", "ggplot2", "ggcorrplot", "sf", "terra", "tmap", 
-                 "tibble", "tidyr", "FD", "lavaan", "semPlot")
+                 "tibble", "tidyr", "FD", "lavaan", "semPlot", "patchwork")
 
 for (i in 1:length(mylibraries)) {
   if(mylibraries[i] %in% rownames(installed.packages()) == FALSE) {install.packages(mylibraries[i])}
